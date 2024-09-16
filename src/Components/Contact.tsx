eb2cbcf635c29426a37dcb656cc5eb507fb0b79d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-16 23:11:55 +0100

    Created Contact.tsx

---
+++ src/Components/Contact.tsx
@@ -0,0 +1,12 @@
+import SocialMedia from "./SocialMedia";
+
+function Contact() {
+    return ( 
+        <div className="Contact">
+            <h2>I am contact</h2>
+            <SocialMedia />
+        </div>
+     );
+}
+
+export default Contact;
