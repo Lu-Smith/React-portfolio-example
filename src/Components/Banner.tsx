31f296f56806af68996f486938d42b379fcfae0b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-17 17:24:28 +0100

    Updated Banner.tsx

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -1,4 +1,8 @@
-import Violin1 from "../assets/violin1.jpg"
+import Violin1 from "../assets/violin1.jpg";
+import Violin2 from "../assets/violin3.jpg";
+import Violin3 from "../assets/violin-solo1.jpg";
+import Violin4 from "../assets/violin-solo2.jpg";
+import Violin5 from "../assets/violin-solo3.jpg";
 
 function Banner () {
     return ( 
@@ -6,13 +10,22 @@ function Banner () {
             <div className="boxTop">
                 <div className="boxLeft">
                     <h1>Portfolio</h1>
-                    <img src={Violin1} alt="violin 1" />
+                    {/* <img src={Violin3} alt="violin 3" />
+                    <img src={Violin4} alt="violin 4" />
+                    <img src={Violin5} alt="violin 5" /> */}
                 </div>
-                <div className="boxRight"></div>
+                <div className="boxRight">
+                    {/* <img src={Violin2} alt="violin 1" /> */}
+                </div>
             </div>
             <div className="boxBottom">
-                <div className="boxLeft"></div>
-                <div className="boxRight"></div>
+                <div className="boxLeft">
+                    {/* <img src={Violin1} alt="violin 2" /> */}
+                </div>
+                <div className="boxRight">
+                
+                    {/* <img src={Violin4} alt="violin 4" /> */}
+                </div>
             </div>
         </div>
      );
