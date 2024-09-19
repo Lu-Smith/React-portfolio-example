48d90d42f0bb05640051985487d7a782e1caea67
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-19 23:01:28 +0100

    Updated layout inBanner.tsx

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -1,5 +1,5 @@
 import Violin3 from "../assets/violin-solo1.jpg";
-import Violin4 from "../assets/violin-solo2.jpg";
+import Violin4 from "../assets/violin3.jpg";
 import Violin5 from "../assets/violin-solo3.jpg";
 
 function Banner () {
@@ -10,10 +10,10 @@ function Banner () {
                     <img src={Violin3} alt="violin 3" />
                 </div>
                 <div className="box number-box non-mobile">1</div>
-                <div className="box imgSmall non-mobile">
+                <div className="box imgSmall non-mobile-img">
                     <img src={Violin5} alt="violin 5" />
                 </div>
-                <div className="box number-box">2</div>
+                <div className="box number-box message">2</div>
                 <div className="description"></div>
                 <div className="box imgLarge">
                     <img src={Violin4} alt="violin 4" />
